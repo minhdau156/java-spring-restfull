414a50758bca9d7f0ae0b0f7a01fef34350169f6
Nguyen Vo Nghiem Minh <nguyenvonghiemminh156@gmail.com>
2025-07-05 20:26:06 +0700

    Bai 48 Chuong 7

--- src/main/java/vn/hoidanit/jobhunter/JobhunterApplication.java
+++ src/main/java/vn/hoidanit/jobhunter/JobhunterApplication.java
@@ -3,10 +3,12 @@ package vn.hoidanit.jobhunter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(exclude = {
-        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
-        org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class}
-        )
+// @SpringBootApplication(exclude = {
+//         org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+//         org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class}
+//         )
+
+@SpringBootApplication
 public class JobhunterApplication {
 
 	public static void main(String[] args) {
